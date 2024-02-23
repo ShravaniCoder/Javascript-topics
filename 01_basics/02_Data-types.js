90abb149a74e07191e3594a887c488e261aeb1f2
Shravani Chendwankar <132210516+ShravaniCoder@users.noreply.github.com>
2024-02-23 05:10:14 +0000

    datatypes

---
+++ 01_basics/02_Data-types.js
@@ -0,0 +1,13 @@
+let name = "hitesh"
+let age = 18
+let isLoggedIn = true
+
+// number 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
