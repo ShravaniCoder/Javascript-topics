82cf506c4f2baea8cb97b9939539e09f94951fa1
Shravani Chendwankar <132210516+ShravaniCoder@users.noreply.github.com>
2024-02-23 05:28:12 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14680
+let accountEmail = "shravani@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "sharavni@google.com"
+accountPassword = "23456"
+accountCity = "Mumbai"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
