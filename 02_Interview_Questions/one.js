802d372662e94ab3dd64571a93d97cfeba4ec622
Shravani Chendwankar <132210516+ShravaniCoder@users.noreply.github.com>
2024-03-07 10:52:15 +0000

    Zero

---
+++ 02_Interview_Questions/one.js
@@ -0,0 +1,27 @@
+// What is Javascript?
+// Javascript is a programminng launguage, that is used for converting static web pages to interactive and dynamic web pages.
+
+//What are data types in javascript? 
+// A data type determines the type of values that can be stored in a variable.
+// There ara two types. Primitive data types and non-primitive data types.
+
+// Primitive data types
+// 1. Number
+let age = 30;
+// 2. String
+let message = 'Hello!';
+// 3. Boolean
+let isTrue = true;
+// 4. Undefined
+let x;
+console.log(x);
+// 5. Null
+let y = null;
+console.log(null);
+
+// Non-primitive data types
+// 1. Object
+// 2. Array
+// 3. Function
+// 4. Date
+// 5. Reg.Exp
