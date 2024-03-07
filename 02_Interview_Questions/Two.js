61333872238c97f1998b7e4a315002f11d355b6d
Shravani Chendwankar <132210516+ShravaniCoder@users.noreply.github.com>
2024-03-07 11:04:43 +0000

    Two

--- 02_Interview_Questions/Two.js
+++ 02_Interview_Questions/Two.js
@@ -12,3 +12,17 @@ function add(a, b){
     return a + b
 }
 console.log(add(2,3))
+
+// Object
+let person = {
+    name: "john",
+    age: 30,
+    grades: ['A', 'B', 'C'],
+    greet: function(){
+        console.log(this.name);
+    }
+};
+
+console.log(person.name);
+console.log(person.grades[1])
+person.greet();
