4bc617165f66bcd4c553f1827ce0d413714520de
Shravani Chendwankar <132210516+ShravaniCoder@users.noreply.github.com>
2024-03-07 10:51:39 +0000

    Two

---
+++ 02_Interview_Questions/Two.js
@@ -0,0 +1,14 @@
+// What are array, functions and objects?
+
+// Array
+let animals = ["Dog", "Cat"]
+console.log(animals[1]);
+console.log(animals.length)
+animals.push("Fox")
+console.log(animals)
+
+// Function
+function add(a, b){
+    return a + b
+}
+console.log(add(2,3))
