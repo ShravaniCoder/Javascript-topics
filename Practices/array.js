c3b12b2f8f035ca3e1a91dc1069a2c90efd01073
Shravani Chendwankar <shravanichendwankar@gmail.com>
2024-04-29 15:11:00 +0530

    yes

---
+++ Practices/array.js
@@ -0,0 +1,9 @@
+function findMax(arr) {
+    if (arr.length === 0) {
+        return "Array is empty"
+    }
+    return Math.max(...arr)
+}
+
+const numbers = [3, 7, 2, 8, 10, 5];
+console.log("Maximum numbers:", findMax(numbers))
