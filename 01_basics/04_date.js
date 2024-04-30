8182a1d747f02f6d5441aa30f545bd313d533d4e
Shravani Chendwankar <shravanichendwankar@gmail.com>
2024-04-30 12:10:37 +0530

    yes

--- 01_basics/04_date.js
+++ 01_basics/04_date.js
@@ -9,3 +9,11 @@ console.log(myDate.toLocaleString()) // Output is 30/04/2024, 09:28:51.
 console.log(myDate.toTimeString()) // Output is 09:36:27 GMT+0530 (India Standard Time).
 console.log(myDate.toUTCString()) // Output is Tue, 30 Apr 2024 04:06:27 GMT.
 console.log(myDate.getTimezoneOffset()) // Output is -330.
+console.log(typeof myDate) // Output is object.
+
+let myCreatedDate = new Date(2024, 4, 4)
+console.log(myCreatedDate.toDateString()) // Output is Sat May 04 2024.
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime())
