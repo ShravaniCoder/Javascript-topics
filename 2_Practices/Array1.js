2160c516ed33af8f2d098390d0a99d1071a11a96
Shravani Chendwankar <shravanichendwankar@gmail.com>
2024-05-02 17:10:55 +0530

    yes

--- 2_Practices/Array1.js
+++ 2_Practices/Array1.js
@@ -9,3 +9,12 @@ var newArr = arr.forEach((item, i) => {
 });
 
 console.log(newArr);
+
+// ForEach method output is
+//1index0
+//2index1
+//3index2
+//5index3
+//undefined
+
+// that means our ForEach not returns.
--- 2_Practices/Array2.js
+++ 2_Practices/Array2.js
@@ -1,6 +1,5 @@
 // Map
 
-
 var arr = [1, 2, 3, 5];
 
 var newArr = arr.map((item, i) => {
@@ -10,3 +9,12 @@ var newArr = arr.map((item, i) => {
 });
 
 console.log(newArr);
+
+// Map method output is
+//1index0
+//2index1
+//3index2
+//5index3
+//[ 1, 3, 5, 8 ]
+
+// that means our Map, it returns.
